f8998ffa9a994e7d0f5c8bb0eea821ae2da0efc8
BackToTheStars <nikgrigoryev@gmail.com>
2020-06-10 22:55:37 -0700

    up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  const requestHttp = () => {
+
+    console.log('Load');
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => {
+        setUsers(json);
+        }
+      );
+
+    // axios.get('https://www.rbc.ru')
+    //   .then((response) => console.log(response.data));
+  };
+
+  // вывести данные пользователей в таблицу
+  // возможность добавлять и удалять пользователей
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={requestHttp}>Load</button>
+      <ul>
+        {
+          users.map(el => <li key={el.id}>{el.name}</li>)        }
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
