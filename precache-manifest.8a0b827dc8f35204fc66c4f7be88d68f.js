9d6bfd48c4384d9d24ef2b07828f3c0ab60e8a82
BackToTheStars <nikgrigoryev@gmail.com>
2020-06-13 00:04:20 -0700

    Updates

---
+++ precache-manifest.8a0b827dc8f35204fc66c4f7be88d68f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "833e3892194f14025363d4c9d007375e",
+    "url": "/react-axios/index.html"
+  },
+  {
+    "revision": "c54c9e476ae7eebe6604",
+    "url": "/react-axios/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "dee83e12260d269a1b1c",
+    "url": "/react-axios/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c54c9e476ae7eebe6604",
+    "url": "/react-axios/static/js/2.6a5c14e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-axios/static/js/2.6a5c14e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dee83e12260d269a1b1c",
+    "url": "/react-axios/static/js/main.e23fdce3.chunk.js"
+  },
+  {
+    "revision": "50d9914793b56aba2eea",
+    "url": "/react-axios/static/js/runtime-main.4769f76c.js"
+  }
+]);
