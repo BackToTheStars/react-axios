589bc1a6b2ee12b8f27415205590f7ef67a9241b
BackToTheStars <nikgrigoryev@gmail.com>
2020-06-13 22:08:28 -0700

    Added "search" input field

--- src/App.js
+++ src/App.js
@@ -6,16 +6,24 @@ import { v4 as uuidv4 } from 'uuid';
 
 function App() {
 
+  const searchIcon = <svg className="bi bi-search" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor"
+                          xmlns="http://www.w3.org/2000/svg">
+                          <path fillRule="evenodd"
+                          d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
+                          <path fillRule="evenodd"
+                          d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
+                     </svg>
+
   const columns=['Name', 'Username', 'Email', 'Phone', 'City', 'Website', 'Company'];
   const [users, setUsers] = useState([]);
   const [isAnotherCellEdited, setIsAnotherCellEdited] = useState(false);
 
   const onUserSave = (user) => {
-    const updatedUsers = users.map((el) => {
+    const updatedUsers = users.map((el) => {   // заменить user в общем списке users
       if (el.id === user.id) return {...user};
       else return el;
     });
-    setUsers(updatedUsers);
+    setUsers(updatedUsers);                    // залить новый список users в State
     setIsAnotherCellEdited(false);        // позволяет снова открыть редактирование другой ячейки
   }
 
@@ -58,7 +66,14 @@ function App() {
             <thead className="thead-light">
             <tr>
               <th scope="col">#</th>
-              {columns.map(el => <th key={uuidv4()} scope="col">{el}</th>)}
+              {columns.map((el) =>
+                <th key={uuidv4()} scope="col">{el}
+                  <input className="ml-3" size="12"
+                         value=' search'
+                  >{null}</input>
+                  {/*<span>{searchIcon}</span>*/}
+                </th>
+                )}
             </tr>
             </thead>
             <tbody>
