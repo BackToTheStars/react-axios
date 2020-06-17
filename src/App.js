de7f8d1dec3ca4ab5fc3c1623013b7ff847854b4
BackToTheStars <nikgrigoryev@gmail.com>
2020-06-17 16:32:40 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,26 +6,6 @@ import Search from "./Components/Search";
 import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  // const searchIcon = (
-  //   <svg
-  //     className="bi bi-search"
-  //     width="1em"
-  //     height="1em"
-  //     viewBox="0 0 16 16"
-  //     fill="currentColor"
-  //     xmlns="http://www.w3.org/2000/svg"
-  //   >
-  //     <path
-  //       fillRule="evenodd"
-  //       d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"
-  //     />
-  //     <path
-  //       fillRule="evenodd"
-  //       d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"
-  //     />
-  //   </svg>
-  // );
-
   const columns = [
     "Name",
     "Username",
@@ -153,3 +133,23 @@ export default App;
 
 // вывести данные пользователей в таблицу
 // возможность добавлять и удалять пользователей// возможность добавлять и удалять пользователей
+
+// const searchIcon = (
+//   <svg
+//     className="bi bi-search"
+//     width="1em"
+//     height="1em"
+//     viewBox="0 0 16 16"
+//     fill="currentColor"
+//     xmlns="http://www.w3.org/2000/svg"
+//   >
+//     <path
+//       fillRule="evenodd"
+//       d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"
+//     />
+//     <path
+//       fillRule="evenodd"
+//       d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"
+//     />
+//   </svg>
+// );
